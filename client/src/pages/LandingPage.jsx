1a23aa4a159648545cc56f5eb3e394c61f4835fe
HelixW <shreyas.2000@hotmail.com>
2020-02-19 19:54:08 +0530

    feat: Landing Page

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LandingHeader from '../components/LandingHeader';
+
+const LandingPage = () => (
+  <div className="bg-black h-screen">
+    <LandingHeader />
+  </div>
+);
+
+export default LandingPage;
