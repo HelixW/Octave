4b6c721388dd6a8bfb0b78f6ec781e1ac1e098a6
HelixW <shreyas.2000@hotmail.com>
2020-02-22 15:05:55 +0530

    Main route added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
+import MainPage from './pages/MainPage';
 
 const App = () => (
   <>
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={LandingPage} />
+        <Route path="/main" component={MainPage} />
       </Switch>
     </BrowserRouter>
   </>
---
+++ client/src/pages/MainPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MainPage = () => <div className="bg-black h-full cursor-default"></div>;
+
+export default MainPage;
