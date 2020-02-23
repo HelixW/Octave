38b04295ced4b6ae8aa198b0849b4cf52e230013
HelixW <shreyas.2000@hotmail.com>
2020-02-23 14:27:57 +0530

    Added logic for dropdown

--- client/src/components/SongCard.jsx
+++ client/src/components/SongCard.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SongCard = ({ styles }) => {
-  console.log(styles);
-  return (
-    <div className={`bg-faded px-12 py-6 ${styles}`}>
-      <div className="text-white song-title text-2xl">Yummy</div>
-      <div className="text-faded text-xl">Justin Bieber</div>
-    </div>
-  );
-};
+const SongCard = ({ styles }) => (
+  <div className={`bg-faded px-12 py-6 ${styles}`}>
+    <div className="text-white song-title text-2xl">Yummy</div>
+    <div className="text-faded text-xl">Justin Bieber</div>
+  </div>
+);
 
 export default SongCard;
 
--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Arrow from './Arrow';
 
 const UserInfo = () => (
-  <div className="flex justify-end items-center cursor-pointer w-1/3 username">
+  <div className="flex justify-end items-center cursor-pointer username">
     <span className="text-xl mx-5">John Doe</span>
     <span>
       <Arrow />
