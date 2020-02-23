6c220468ac3da515afe34f6d28d50f00fd16ea85
HelixW <shreyas.2000@hotmail.com>
2020-02-23 11:10:43 +0530

    Added anchor tags

--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -4,12 +4,12 @@ const SearchBox = () => (
   <div className="mb-5 py-2 flex flex-stretch w-2/3">
     <input
       type="text"
-      className="h-full bg-secondary px-8 py-0 text-xl text-white focus:shadow-outline box-border w-11/12"
+      className="h-full bg-secondary px-8 py-0 text-xl text-white box-border w-11/12"
       placeholder="Add a Song to Queue"
     />
     <input
       type="submit"
-      className="h-full w-1/12 cursor-pointer text-5xl leading-none focus:shadow-outline bg-contrast"
+      className="h-full w-1/12 cursor-pointer text-5xl leading-none hover:bg-green-700 bg-contrast"
       value="+"
     />
   </div>
--- client/src/components/UserInfo.jsx
+++ client/src/components/UserInfo.jsx
@@ -3,7 +3,7 @@ import Arrow from './Arrow';
 
 const UserInfo = () => (
   <div className="flex justify-end items-center cursor-pointer w-1/3">
-    <span className="text-xl mx-5">John Doe</span>
+    <span className="text-xl mx-5 username">John Doe</span>
     <span>
       <Arrow />
     </span>
