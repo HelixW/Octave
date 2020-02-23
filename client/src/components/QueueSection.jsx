64f99105484b6bbd899dcbb20ba32ddec567376c
HelixW <shreyas.2000@hotmail.com>
2020-02-23 10:17:53 +0530

    Search Bar added

--- client/src/components/QueueSection.jsx
+++ client/src/components/QueueSection.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import SectionHeader from './SectionHeader';
 import SongCard from './SongCard';
+import SearchBox from './SearchBox';
 
 const QueueSection = () => (
   <section className="mt-5 pl-10">
-    <SectionHeader>Queue</SectionHeader>
+    <div className="flex justify-between">
+      <SectionHeader>Queue</SectionHeader>
+      <SearchBox />
+    </div>
     <div className="overflow-y-scroll h-128">
       <SongCard styles="mb-1" />
       <SongCard styles="mb-1" />
